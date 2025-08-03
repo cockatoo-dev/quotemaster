e5ee3e183b48dd72ee3a9e48f8381fdfd976f3e3
cockatoo-dev <maxyuen8539@gmail.com>
2025-08-04 00:14:54 +1000

    Update packages

--- build-react/tailwind.config.js
+++ build-react/tailwind.config.js
@@ -5,7 +5,7 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@heroui/react/node_modules/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -15,6 +15,6 @@ export default {
     },
   },
   darkMode: 'class',
-  plugins: [heroui({addCommonColors: true})],
+  plugins: [heroui()],
 }
 
